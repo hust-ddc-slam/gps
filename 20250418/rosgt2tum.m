65a8651c845c95e3fdd5e09ddb7a29710e8a5889
LarryDong <jfjxxdy2007@126.com>
2025-04-24 17:45:05 +0800

    Fix gps gt export bugs.

--- 20250418/rosgt2tum.m
+++ 20250418/rosgt2tum.m
@@ -1,32 +1,45 @@
 clc;clear;close all;
 
 %输入rosbag存放文件路径和方法名称
-folderpath = "F:\test";
-methodname = "cticp";
+folderpath = "/home/larry/data/3dof_street_0423/";
+gt_output_path = "/home/larry/data/3dof_street_0423/gt/";
+% methodname = "cticp";     % methodname is useless for gt-generation.
 
 dir = dir(folderpath);%获取原始数据文件夹下所有文件的信息
-raw_folder = folderpath + "\";%原始数据文件夹名
+raw_folder = folderpath + "/";%原始数据文件夹名
 
 for i = 3:(length(dir))%遍历原始数据夹下的文件
     if ~contains(dir(i).name,".bag")    
         continue
     end
-    dataname = dir(i).name;
-    datapath = raw_folder + dataname;
-    savename = "gt_"+methodname;
-    savepath = raw_folder + savename + num2str(i-2)+".txt";
-    bag = rosbag(datapath);
+    [~, seq, ~] = fileparts(dir(i).name);       % extract the sequence name without extension
+    
+    % seq = "04-novib-2";       % test one sequence.
+    rospose = [];                       % clear the trajectory every time.
+
+    bag_filename = seq + ".bag";
+    bag_filename = folderpath + bag_filename;
+
+    fprintf("--> Processing rosbag: %s \n", bag_filename);
+
+    bag = rosbag(bag_filename);
     msgs = select(bag,"Topic", '/gps_node/gps_traj');
     traj = readMessages(msgs,'DataFormat','struct');
     for j = 1:length(traj)
+        traj{j}.Header.Stamp.Sec
         timestamps = double(traj{j}.Header.Stamp.Sec) + double(traj{j}.Header.Stamp.Nsec)*1e-9;
         rospose(j,:) = [timestamps,traj{j}.Pose.Position.X,traj{j}.Pose.Position.Y,traj{j}.Pose.Position.Z];
     end
-
-    fileID = fopen(savepath, 'w');
-
+    
+    gt_filename = "gt_" + seq + ".txt";
+    gt_filename = gt_output_path + gt_filename;
+    fileID = fopen(gt_filename, 'w');
     for k = 1:length(rospose)
     fprintf(fileID, '%.6f %.6f %.6f %.6f %.6f %.6f %.6f %.6f\n', ...
         rospose(k,1),rospose(k,2), rospose(k,3), rospose(k,4), 0, 0, 0, 1);
     end
+
+    fclose(fileID);             % close the file everytime.
+    fprintf("<-- Saved gt to file: %s \n", gt_filename);
+
 end
